a49b1b77c7a803e12640c91c9702a5c7ec5b772d
Frans Imanuel <fransimanuel99@gmail.com>
2024-08-09 14:56:38 +0700

    Update

--- module/tr_tasklist/handler/tr_tasklist_handler.go
+++ module/tr_tasklist/handler/tr_tasklist_handler.go
@@ -4,6 +4,7 @@ import (
 	"archive/zip"
 	"context"
 	"encoding/base64"
+	"errors"
 	"fmt"
 	"io"
 	"io/fs"
@@ -34,100 +35,100 @@ func (controller *TRTasklistHandler) ExportPDFTasklistBulk() {
 	IDs := controller.TRTasklistService.BulkExportOneTimeService()
 	var wg sync.WaitGroup
 
-	// errCh := make(chan error, 1)
-	// ctxG, cancel := context.WithCancel(context.Background())
-	// defer cancel()
+	errCh := make(chan error)
+	ctxG, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
 	// panic(len(IDs))
 	fmt.Println("len(IDs): ", len(IDs))
 	wg.Add(len(IDs))
 	for _, ID := range IDs {
-		// go func(id string) {
+		go func(id string) {
 
-		// 	defer wg.Done()
+			defer wg.Done()
 
-		// 	select {
-		// 	case <-ctxG.Done():
-		// 		return
-		// 	default:
-		// 		Base64, fname, errSrv := controller.TRTasklistService.ExportPDFTasklist(context.Background(), dto.GetTasklistByID{TasklistId: id})
+			select {
+			case <-ctxG.Done():
+				return
+			default:
+				Base64, fname, errSrv := controller.TRTasklistService.ExportPDFTasklist(context.Background(), dto.GetTasklistByID{TasklistId: id})
 
-		// 		fmt.Println("Base64: ", *Base64)
-		// 		cancel() // Cancel all other goroutines
-		// 		return
+				fmt.Println("Base64: ", *Base64)
+				cancel() // Cancel all other goroutines
+				return
 
-		// 		if errSrv.Message != "" {
-		// 			errCh <- errors.New(errSrv.Message)
-		// 			cancel() // Cancel all other goroutines
-		// 			return
-		// 		}
-		// 		dec, errFile := base64.StdEncoding.DecodeString(*Base64)
-		// 		if errFile != nil {
-		// 			errCh <- errFile
-		// 			cancel() // Cancel all other goroutines
-		// 			return
-		// 		}
+				if errSrv.Message != "" {
+					errCh <- errors.New(errSrv.Message)
+					cancel() // Cancel all other goroutines
+					return
+				}
+				dec, errFile := base64.StdEncoding.DecodeString(*Base64)
+				if errFile != nil {
+					errCh <- errFile
+					cancel() // Cancel all other goroutines
+					return
+				}
 
-		// 		f, err := os.Create(fname)
-		// 		if err != nil {
-		// 			errCh <- err
-		// 			cancel() // Cancel all other goroutines
-		// 			return
-		// 		}
-		// 		defer f.Close()
+				f, err := os.Create(fname)
+				if err != nil {
+					errCh <- err
+					cancel() // Cancel all other goroutines
+					return
+				}
+				defer f.Close()
 
-		// 		if _, err := f.Write(dec); err != nil {
-		// 			errCh <- err
-		// 			cancel() // Cancel all other goroutines
-		// 			return
-		// 		}
-		// 		if err := f.Sync(); err != nil {
-		// 			errCh <- err
-		// 			cancel() // Cancel all other goroutines
-		// 			return
-		// 		}
-		// 	}
+				if _, err := f.Write(dec); err != nil {
+					errCh <- err
+					cancel() // Cancel all other goroutines
+					return
+				}
+				if err := f.Sync(); err != nil {
+					errCh <- err
+					cancel() // Cancel all other goroutines
+					return
+				}
+			}
 
-		// }(ID)
+		}(ID)
 
-		Base64, fname, errSrv := controller.TRTasklistService.ExportPDFTasklist(context.Background(), dto.GetTasklistByID{TasklistId: ID})
-		if errSrv.Message != "" {
-			fmt.Println("err: ", errSrv.Message)
-		}
-		dec, errFile := base64.StdEncoding.DecodeString(*Base64)
-		if errFile != nil {
-			// panic(errFile)
-			fmt.Println("err: ", errFile)
-			return
-		}
+		// Base64, fname, errSrv := controller.TRTasklistService.ExportPDFTasklist(context.Background(), dto.GetTasklistByID{TasklistId: ID})
+		// if errSrv.Message != "" {
+		// 	fmt.Println("err: ", errSrv.Message)
+		// }
+		// dec, errFile := base64.StdEncoding.DecodeString(*Base64)
+		// if errFile != nil {
+		// 	// panic(errFile)
+		// 	fmt.Println("err: ", errFile)
+		// 	return
+		// }
 
-		f, err := os.Create(fname)
-		if err != nil {
-			fmt.Println("err: ", err)
-			return
-		}
-		defer f.Close()
+		// f, err := os.Create(fname)
+		// if err != nil {
+		// 	fmt.Println("err: ", err)
+		// 	return
+		// }
+		// defer f.Close()
 
-		if _, err := f.Write(dec); err != nil {
-			fmt.Println("err: ", err)
-			return
-		}
-		if err := f.Sync(); err != nil {
-			fmt.Println("err: ", err)
-			return
-		}
+		// if _, err := f.Write(dec); err != nil {
+		// 	fmt.Println("err: ", err)
+		// 	return
+		// }
+		// if err := f.Sync(); err != nil {
+		// 	fmt.Println("err: ", err)
+		// 	return
+		// }
 
 	}
 
-	// wg.Wait()
-	// close(errCh)
+	wg.Wait()
+	close(errCh)
 
-	// for err := range errCh {
-	// 	if err != nil {
-	// 		log.Printf("Encountered error: %v", err)
-	// 		return
-	// 	}
-	// }
+	for err := range errCh {
+		if err != nil {
+			log.Printf("Encountered error: %v", err)
+			return
+		}
+	}
 
 	log.Println("All tasks completed successfully")
 	fmt.Println("len(IDs): ", len(IDs))
@@ -136,9 +137,9 @@ func (controller *TRTasklistHandler) ExportPDFTasklistBulk() {
 	// ZIP PROGRESS
 
 	// Define the folder to zip and the output zip file
-	folderToZip := "storage" // The folder you want to zip
-	// outputZipFile := "MF_TASKLIST-05.zip" // The name of the output zip file
-	outputZipFile := "MF_TASKLIST-06.zip" // The name of the output zip file
+	folderToZip := "storage"              // The folder you want to zip
+	outputZipFile := "MF_TASKLIST-05.zip" // The name of the output zip file
+	// outputZipFile := "MF_TASKLIST-06.zip" // The name of the output zip file
 	err := ZipFolder(folderToZip, outputZipFile)
 	if err != nil {
 		fmt.Println("Error:", err)
